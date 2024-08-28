5714f647c84b39284bafba67651ad0d05503eeb7
JJ Perez <gurufractal@hotmail.com>
2024-08-28 18:37:19 -0400

    Se cambio el código python para adaptarlo al calculo de media movil exponencia (EMA) la cual se ajusta mejor a los cambios volatiles en la estrategia de Scalpin

--- src/trader_bot.py
+++ src/trader_bot.py
@@ -12,19 +12,25 @@ def obtener_tikckers():
         price = tiker['price']
         print("Simbolo: " + symbol + ", Precio: " + price)
 #obtener_tikckers()
-def velas():   
+def EMA_INTERVALO_5MIN(periodo, ticker):   
     lista_precios_cierre = []
-    data_historical = client.get_historical_klines("DARUSDT",Client.KLINE_INTERVAL_4HOUR,"804 hour ago UTC")
+    data_historical = client.get_historical_klines(ticker,Client.KLINE_INTERVAL_5MINUTE,"250 minutes ago UTC")
 
-    #print("Cantidad de velas: " , len(data_historical))
+#    print("Cantidad de velas: " , len(data_historical))
     sumatoria = 0
-    if len(data_historical) == 201:
+    if len(data_historical) == 50:
       print("Se obtuvieron los datos correctamente")
-      for i in range((201-200), 200):
+      for i in range((50-periodo), 50):
          #print(data_historical[i])
          sumatoria += float(data_historical[i][4])
-      sma = sumatoria/200
-      print("SMA 200 DARUSDT: ", sma)
+         precio_cierre_actual = float(data_historical[i][4])
+      sma = sumatoria/periodo
+      multiplicador = 2/(periodo + 1)
+      EMA = sma
+      EMA = (precio_cierre_actual - EMA) * multiplicador + EMA
+      print("EMA: " + ticker + " periodo " + str(periodo) + " : " +str(EMA))
+      return(EMA)
     else:
         print("No se pudo obtener el historial de velas") 
-velas()
+EMA_INTERVALO_5MIN(5,'BNXUSDT')
+EMA_INTERVALO_5MIN(25,'BNXUSDT')
